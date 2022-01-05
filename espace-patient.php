85aba38376098486091c0077266b2044df5b9a4c
Alice Goarnisson <alice.goarnisson@gmail.com>
2022-01-05 15:01:35 +0100

    espace patient et ordonnance modifiée

---
+++ espace-patient.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Générer ordonnance</title>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+    />
+    <link rel="stylesheet" href="ordonnance.css" />
+  </head>
+  <body>
+    <section class="title">Espace Patient</section>
+    <div class="col-md-6 text-center">
+    <button type="button" class="btn btn-primary" href="#">QR Code informations médicales</button>
+    <button type="button" class="btn btn-primary" href="#">Scan</button>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
