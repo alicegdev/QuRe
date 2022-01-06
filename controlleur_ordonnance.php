35d2a3c389d92927df500643e1cfcf7dfdc029a6
Narva78 <64193035+Narva78@users.noreply.github.com>
2022-01-06 13:53:06 +0100

    Update controlleur_ordonnance.php

--- controlleur_ordonnance.php
+++ controlleur_ordonnance.php
@@ -7,14 +7,27 @@ $idMedecin = $pdo->getIdMed();
 
 switch($action) {
     case 'genererOrdonnance':{
+        $Medicament = $_REQUEST['medic']; #medicament
+        $Quantite = $_REQUEST['quantite']; #quantite de medicament
+        $frequence = $_REQUEST['frequence']; #frequence de prise de medcament => 2*/j
+        $echelleTemps = $_REQUEST['echelleTemps']; #jours, semaine ....
+        $commedic = $_REQUEST['commedic']; #Commentaire Medicament (du traitement) => condition d'utilisation
+        $comglobal = $_REQUEST['comglobal']; #Commentaire global de l'ordonnane
         $test1 = $pdo->setOrdonnance($_POST['comglobal'], $_POST['datefin']);
         include("ordonnance.php");
         include("v_erreurs.php");
     }
 
     case 'affichageOrdonnancePdf':{
+        $Medicament = $_REQUEST['medic']; #medicament
+        $Quantite = $_REQUEST['quantite']; #quantite de medicament
+        $frequence = $_REQUEST['frequence']; #frequence de prise de medcament => 2*/j
+        $echelleTemps = $_REQUEST['echelleTemps']; #jours, semaine ....
+        $commedic = $_REQUEST['commedic']; #Commentaire Medicament (du traitement) => condition d'utilisation
+        $comglobal = $_REQUEST['comglobal']; #Commentaire global de l'ordonnane
         $idTraitement=$pdo->getIdTraitement();
         $idOrdonnance=$pdo->getIdOrdonnance($idTraitement, $idPatient, $idMedecin);
+        include("ordonnance.php");
         include("ordonnance.php");
 
     }
